e902b62fd8ecc6337e98435d7be3ed15b6211f18
agwata9 <agwata9@gmail.com>
2023-05-06 13:08:51 +0000

    correction & submission

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,8 @@
 --create table
 -- If table not exist
 CREATE TABLE IF NOT EXISTS users (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(255) NOT NULL UNIQUE,
-  name VARCHAR(255),
-  PRIMARY KEY (id)
+  name VARCHAR(255)
 );
 
--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
 -- SQL script that lists all bands with Glam rock
-SELECT band_name, (YEAR(split) - YEAR(formed)) AS lifespan
+SELECT band_name, (IFNULL(split, 2022) - formed)
+AS lifespan
 FROM metal_bands
-WHERE main_style = 'Glam rock'
+WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
